e5389d196d2f6fbddd1c054a0d1f46a502c53c42
Daniel Neset <danielneset@gmail.com>
2024-09-27 21:18:54 +0200

    Added expense widgets, models and lists

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # expense_tracker
+
 Unity tutorial to create a expense tracker mobile app
+
+### Requirements
+
+pub package uuid.
---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 16,
+      ),
+      child: Column(
+        children: [
+          Text(expense.title),
+          const SizedBox(height: 4),
+          Row(
+            children: [
+              // value like 12.123123 will be displayed as 12.12
+              Text('\$${expense.amount.toStringAsFixed(2)}'),
+              const Spacer(),
+              Row(
+                children: [
+                  const Icon(Icons.alarm),
+                  const SizedBox(width: 8),
+                  Text(expense.date.toString()),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+}
